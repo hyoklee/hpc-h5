01533ecfa7721d29889bcc7c98f05a5743d7762d
H. Joe LEE <hyoklee@hdfgroup.org>
2023-04-20 17:12:47 -0400

    ci: update job name for Frontier

--- bin/cron_fr.sh
+++ bin/cron_fr.sh
@@ -8,7 +8,7 @@ OLD_HEAD=$(git rev-parse HEAD)
 git pull
 NEW_HEAD=$(git rev-parse HEAD)
 [ $OLD_HEAD = $NEW_HEAD ] && exit 0
-cd /ccs/home/hyoklee/src
-sbatch h5.slurm
+cd /ccs/home/hyoklee/src/hpc-h5/bin
+sbatch j_fr.slurm
 sleep 1200
 cd /lustre/orion/csc332/scratch/hyoklee/hdf5/build && ctest -T Submit
