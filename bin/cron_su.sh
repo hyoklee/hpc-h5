227af6fa34dc334c596fa5b7f2ed89071561a2a1
H. Joe Lee <hyoklee@hdfgroup.org>
2023-06-08 15:46:26 +0000

    ci: build with cron on Sunspot

--- bin/cron_su.sh
+++ bin/cron_su.sh
@@ -6,12 +6,18 @@
 echo "Hello" > /home/hyoklee/bin/hello.txt
 module load daos/base
 
-cd /lus/gila/projects/CSC250STDM10_CNDA/hyoklee/hdf5
+d="/lus/gila/projects/CSC250STDM10_CNDA/hyoklee/hdf5"
+cd $d
 /home/hyoklee/bin/ckrev
 rc_h5=$?
 
 if [ $rc_h5 -eq 1 ]
 then
+   rm -rf $d/build
+   mkdir $d/build
+   cd $d/build
+   /home/hyoklee/src/hpc-h5/bin/cmake.sh
+   ctest -T Build --output-on-error -j
    cd /home/hyoklee/src/hpc-h5/bin
    qsub j_su.pbs
 fi
