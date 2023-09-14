dd0a3cbcfd29353626f3db29a1547e872bc818e9
H. Joe Lee <hyoklee@hdfgroup.org>
2023-09-14 14:50:54 -0700

    ci: add perlmutter scripts

---
+++ bin/cron_pe.sh
@@ -0,0 +1,25 @@
+#!/usr/bin/bash
+
+. /etc/profile
+. /global/homes/h/hyoklee/.bashrc
+
+echo "Hello" > /global/homes/h/hyoklee/bin/hello.txt
+module load nvhpc/23.1
+
+d="/pscratch/sd/h/hyoklee/hdf5"
+cd $d
+/global/homes/h/hyoklee/src/hpc-h5/bin/ckrev
+rc_h5=$?
+
+if [ $rc_h5 -eq 1 ]
+then
+   rm -rf $d/build
+   mkdir $d/build
+   cd $d/build
+   /global/homes/h/hyoklee/src/hpc-h5/bin/cmake_nv_pe.sh
+   sbatch j_pe.slurm
+fi
+
+# To measure time
+echo "Hello2" > /home/hyoklee/bin/hello2.txt
+cd /pscratch/sd/h/hyoklee/hdf5/build && ctest -T Submit
