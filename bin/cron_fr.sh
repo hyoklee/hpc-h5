b35a057bb87c73529cefcc16f6869f6153181de5
H. Joe LEE <hyoklee@hdfgroup.org>
2023-10-24 10:16:46 -0400

    ci: update frontier job scripts

--- bin/cron_fr.sh
+++ bin/cron_fr.sh
@@ -2,13 +2,22 @@
 
 . /etc/profile
 . /ccs/home/hyoklee/.bashrc
-
-cd /lustre/orion/csc332/scratch/hyoklee/hdf5
-OLD_HEAD=$(git rev-parse HEAD)
-git pull
-NEW_HEAD=$(git rev-parse HEAD)
-[ $OLD_HEAD = $NEW_HEAD ] && exit 0
-cd /ccs/home/hyoklee/src/hpc-h5/bin
-sbatch j_fr.slurm
-sleep 1200
-cd /lustre/orion/csc332/scratch/hyoklee/hdf5/build && ctest -T Submit
+echo "Hello" > /ccs/home/hyoklee/tmp/hello.txt
+d="/lustre/orion/csc332/scratch/hyoklee/hdf5"
+cd $d
+rm -rf $d/build
+mkdir $d/build
+cd $d/build
+cmake \
+    -D BUILDNAME:STRING=cce \
+    -D CTEST_DROP_SITE_INIT:STRING="my.cdash.org" \
+    -D HDF5_BUILD_FORTRAN:BOOL=ON \
+    -D HDF5_ENABLE_MAP_API:BOOL=ON \
+    -D HDF5_ENABLE_PARALLEL:BOOL=ON \
+    -D HDF5_ENABLE_SUBFILING_VFD:BOOL=ON \
+    -D HDF5_ENABLE_SZIP_SUPPORT:BOOL=OFF \
+    -D HDF5_ENABLE_Z_LIB_SUPPORT:BOOL=ON \
+    -D SITE:STRING=frontier \
+    ..
+sbatch /ccs/home/hyoklee/src/hpc-h5/bin/j_fr.slurm
+echo "Hello2" > /ccs/home/hyoklee/tmp/hello2.txt
