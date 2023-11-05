8046e0b623d836a8942129398aa934859612ab90
H. Joe LEE <hyoklee@hdfgroup.org>
2023-11-05 16:12:28 -0500

    fix(fr): submit test result from login node
    close #2

--- bin/cron_fr.sh
+++ bin/cron_fr.sh
@@ -3,23 +3,26 @@
 . /etc/profile
 . /ccs/home/hyoklee/.bashrc
 echo "Hello" > /ccs/home/hyoklee/tmp/hello.txt
-d="/lustre/orion/csc332/scratch/hyoklee/hdf5"
+d="/lustre/orion/csc332/scratch/hyoklee/"
 cd $d
-rm -rf $d/build
-mkdir $d/build
-cd $d/build
+rm -rf hdf5
+git clone https://github.com/HDFGroup/hdf5
+cd hdf5
+mkdir build
+cd build
 cmake \
     -D BUILDNAME:STRING=cce \
     -D CTEST_DROP_SITE_INIT:STRING="my.cdash.org" \
-    -D CTEST_TEST_TIMEOUT:STRING="300" \
     -D DART_TESTING_TIMEOUT:STRING="300" \
     -D HDF5_BUILD_FORTRAN:BOOL=ON \
-    -D HDF5_ENABLE_MAP_API:BOOL=ON \
+    -D HDF5_ENABLE_MAP_API:BOOL=OFF \
     -D HDF5_ENABLE_PARALLEL:BOOL=ON \
-    -D HDF5_ENABLE_SUBFILING_VFD:BOOL=ON \
+    -D HDF5_ENABLE_SUBFILING_VFD:BOOL=OFF \
     -D HDF5_ENABLE_SZIP_SUPPORT:BOOL=OFF \
     -D HDF5_ENABLE_Z_LIB_SUPPORT:BOOL=ON \
     -D SITE:STRING=frontier \
     ..
 sbatch /ccs/home/hyoklee/src/hpc-h5/bin/j_fr.slurm
+sleep 1200
+ctest -T Submit
 echo "Hello2" > /ccs/home/hyoklee/tmp/hello2.txt
--- bin/j_fr.sh
+++ bin/j_fr.sh
@@ -2,5 +2,5 @@
 echo "Running CTest"
 cd /lustre/orion/csc332/scratch/hyoklee/hdf5/build
 ctest -T Build --output-on-error -j
-ctest -T Test --output-on-error --timeout 300 -j
-ctest -T Submit
+ctest -T Test -E MPI --output-on-error --timeout 300 -j
+
